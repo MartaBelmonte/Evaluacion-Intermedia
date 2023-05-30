e505b9a6ec5d7353b5387861088fff6345885baa
MartaBelmonte <martabelmonts5@gmail.com>
2023-05-30 12:50:30 +0200

    variables

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+//variables
+const button = document.querySelector (.js_updatebottom);
+const face = document.querySelector (.js_faceh1);
+const main = document.querySelector (.js_main);
+const section = document.querySelector (.js_section1):
+
+
+
+//funciones
+function changeFace (){
+    const valueFace = section.value;
+    face.innherHTML = valueFace;
+}
+
+function changeColor (){
+    const randomNum = parseInt(Math.random() * 100);
+// const randomNum = Math.round(Math.random() * 100); también es otra manera de redondear
+    console.log (randomNum);
+}
+
+function handleClickButton (event){
+    changeFace();
+    changeColor();
+}
+
+
+//eventos
+button.addEventListener ('click', handleClickButton)
